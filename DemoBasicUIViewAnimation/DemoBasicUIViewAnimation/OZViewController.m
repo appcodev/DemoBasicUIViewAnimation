787985302030e15e98582f334e689c0e865b8ca8
Chalermchon Samana <appcodev@gmail.com>
2013-10-16 04:13:23 +0700

    ok v.2

--- DemoBasicUIViewAnimation/DemoBasicUIViewAnimation/OZViewController.m
+++ DemoBasicUIViewAnimation/DemoBasicUIViewAnimation/OZViewController.m
@@ -28,31 +28,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)animateGrim:(id)sender {
+- (IBAction)animateGrim:(UIButton*)sender {
     //animate grim
-    //NSLog(@"animation key : %@",self.grimImage.layer.animationKeys);
-    if (self.grimImage.layer.animationKeys==nil) {
+    [self animateGrimWithCheckAnimateRunning:sender.tag==1];
+}
+
+- (void)animateGrimWithCheckAnimateRunning:(BOOL)check{
+    //if checked, we check animationKeys not null to start new animation ...
+    BOOL allow = check?self.grimImage.layer.animationKeys==nil:YES;
+    if (allow) {
         [UIView animateWithDuration:2
                          animations:^{
                              self.grimImage.transform = CGAffineTransformMakeScale(0.2, 0.2);
                              self.grimImage.transform = CGAffineTransformMakeRotation(M_PI);
-                             
-                             
                          }
                          completion:^(BOOL finished){
-                             //if (finished) {
-                             //   NSLog(@"finished...");
+                             
+                             //start animation reverse
                              [UIView animateWithDuration:2
                                               animations:^{
+                                                  //to default transform ...
                                                   self.grimImage.transform = CGAffineTransformIdentity;
                                               }];
                              
-                             //}else{
-                             //    NSLog(@"not finished");
-                             //}
                          }];
     }
-    
+
 }
 
 @end
